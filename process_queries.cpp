39e0018e49bc5152f6db599f4f832f4a647f0671
Рустам Нурмиев <Rustam2202@yandex.ru>
2022-01-25 19:20:05 +0300

    v0.2

--- process_queries.cpp
+++ process_queries.cpp
@@ -1,4 +1,4 @@
-#include "process_queries.h"
+﻿#include "process_queries.h"
 
 #include <algorithm>
 #include <iterator>
@@ -7,33 +7,19 @@
 #include <utility>
 
 std::vector<std::vector<Document>> ProcessQueries(const SearchServer& search_server, const std::vector<std::string>& queries) {
-
 	std::vector<std::vector<Document>> result(queries.size());
 	std::transform(std::execution::par, std::make_move_iterator(queries.begin()), std::make_move_iterator(queries.end()), std::make_move_iterator(result.begin()),
 		[&search_server](const std::string& query) {return std::move(search_server.FindTopDocuments(query)); });
-
 	return result;
 }
 
 std::list<Document> ProcessQueriesJoined(const SearchServer& search_server, const std::vector<std::string>& queries) {
-
 	std::list<Document> result;
-	std::vector<Document> doc;
-	result.splice(result.begin(), doc.begin());
-
-
-	/*std::transform(std::execution::par, std::make_move_iterator(queries.begin()), std::make_move_iterator(queries.end()), std::make_move_iterator(result.begin()),
-		[&search_server](const std::string& query) {return std::move(search_server.FindTopDocuments(query)); });*/
-
+	std::vector<std::vector<Document>> vect_of_docs = ProcessQueries(search_server, queries);
+	for (std::vector<Document> docs : vect_of_docs) {
+		for (Document doc : docs) {
+			result.push_back(std::move(doc));
+		}
+	}
 	return result;
 }
-
-//std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server, const std::vector<std::string>& queries) {
-//	std::vector<Document> result;
-//	for (auto query : queries) {
-//		for (auto doc : search_server.FindTopDocuments(std::move(query))) {
-//			result.push_back(std::move(doc));
-//		}
-//	}
-//	return result;
-//}
